b66ec8c59947e148a9eddc5939a7b5f3111c3e76
Jason Liu <jliu2383@uwo.ca>
2020-05-15 19:47:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Coming Soon...</h1>
+        <p>Hi! I'm Jason</p>
       </header>
     </div>
   );
