d22ef1c80fde420713ce06e20090feedb650f641
Jason Liu <jliu2383@uwo.ca>
2020-05-17 22:09:25 -0400

    gh pages test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import img from './fufu.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coming Soon...</h1>
-        <p>Hi! I'm Jason</p>
-        <p>Update test 2</p>
+        <h className="Top-header">how do i even build website</h>
+        <img src={img} className="Joel" alt="sadBoi" height="60%"/>
       </header>
     </div>
   );
