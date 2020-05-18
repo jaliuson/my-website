dea4091be2f479d90d8315e3f2221d7cf3c48ee1
Jason Liu <jliu2383@uwo.ca>
2020-05-18 14:42:49 -0400

    started navbar

---
+++ src/NavigationBar/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <div className="NavBar">
+                <h1 className="Text">Jason Liu</h1>
+            </div>
+        )
+    }
+}
+
+export default NavBar
