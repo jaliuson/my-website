93e9c508411a74a8cb66b054b9f56e384a37b8cd
Jason Liu <jliu2383@uwo.ca>
2020-05-23 00:21:00 -0400

    Added content panels and dynamic creation using props
    adding images to content panels will be next step, place holder blocks for now

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -4,18 +4,20 @@ import ContentPanel from './ContentPanel/ContentPanel';
 
 // Paralax Content------------------------------------------
 import { ParallaxProvider } from 'react-scroll-parallax';
-import { Parallax } from 'react-scroll-parallax';
 // Paralax Content------------------------------------------
+
+//Images----------------------------------------------------
+
 
 class App extends Component {
   state = {
     panels: [
-      {title: 'Welcome' , content: 'My Name is Jason'},
-      {title: 'About Me' , content: 'I am entering 3rd year at Western University'},
-      {title: 'My Work' , content: 'I am in sfotware engineering, I works mainly with Java and Javascript (React)'},
-      {title: 'Projects' , content: 'My projects ranging from Android development to machine learning'},
-      {title: 'Interets' , content: 'I enjoy running, cycling and Ultimate'},
-      {title: 'contact' , content: 'Insert Various Links'}
+      {title: 'Hi there,' , content: "I'm Jason" , picture: './Profile.JPG' , direction: 'L'},
+      {title: 'About Me' , content: 'Entering 3rd year at Western University' , picture: "./AboutMe.jpg" , direction: 'R'},
+      {title: 'My Work' , content: 'I am in sfotware engineering, I work with Java and Javascript (React)' , picture: "./Work.jpg" , direction: 'L'},
+      {title: 'Projects' , content: 'My projects ranging from Android development to machine learning' , picture: "./Project1.jpg" , direction: 'R'},
+      {title: 'Interets' , content: 'I enjoy running, cycling and Ultimate' , picture: "./Interest.jpg" , direction: 'L'},
+      {title: 'contact' , content: 'Insert Various Links' , picture: "contact.jpg" , direction: 'R'}
     ]
   }
   
@@ -24,40 +26,41 @@ class App extends Component {
       <ParallaxProvider>
         <div className="App">
           <header className="App-header">
-          <h1>Jason Liu</h1>
-        </header>
-        <body className="Custom">
-        
-
-          <ContentPanel
-            title={this.state.panels[0].title}
-            content={this.state.panels[0].content}
-          ></ContentPanel>
-          <ContentPanel
-            title={this.state.panels[1].title}
-            content={this.state.panels[1].content}
-          ></ContentPanel>
-          <ContentPanel
-            title={this.state.panels[2].title}
-            content={this.state.panels[2].content}
-          ></ContentPanel>
-          <ContentPanel
-            title={this.state.panels[3].title}
-            content={this.state.panels[3].content}
-          ></ContentPanel>
-          <ContentPanel
-            title={this.state.panels[4].title}
-            content={this.state.panels[4].content}
-          ></ContentPanel>
-          <ContentPanel
-            title={this.state.panels[5].title}
-            content={this.state.panels[5].content}
-          ></ContentPanel>
-
-        
-        </body>
-        
-      </div>
+            <h1>Jason Liu</h1>
+          </header>
+          <div className="Custom">
+            <ContentPanel
+              title={this.state.panels[0].title}
+              content={this.state.panels[0].content}
+              direction={this.state.panels[0].direction}
+            ></ContentPanel>
+            <ContentPanel
+              title={this.state.panels[1].title}
+              content={this.state.panels[1].content}
+              direction={this.state.panels[1].direction}
+            ></ContentPanel>
+            <ContentPanel
+              title={this.state.panels[2].title}
+              content={this.state.panels[2].content}  
+              direction={this.state.panels[2].direction}   
+            ></ContentPanel>
+            <ContentPanel
+              title={this.state.panels[3].title}
+              content={this.state.panels[3].content}
+              direction={this.state.panels[3].direction}
+            ></ContentPanel>
+            <ContentPanel
+              title={this.state.panels[4].title}
+              content={this.state.panels[4].content}
+              direction={this.state.panels[4].direction}
+           ></ContentPanel>
+            <ContentPanel
+              title={this.state.panels[5].title}
+              content={this.state.panels[5].content}
+              direction={this.state.panels[5].direction}
+            ></ContentPanel>
+          </div>
+        </div>
       </ParallaxProvider>
     )
   }
--- personal-website/src/ContentPanel/ContentPanel.js
+++ personal-website/src/ContentPanel/ContentPanel.js
@@ -7,14 +7,28 @@ import { Parallax } from 'react-scroll-parallax';
 
 export class ContentPanel extends Component {
     render() {
+        let imageSide = null;
+        let textAnchor = null;
+        if(this.props.direction === 'L'){ //for dynamic styling
+            imageSide = 'row';
+            textAnchor = 'left';
+        }
+        else{
+            imageSide = 'row-reverse';
+            textAnchor = 'right';
+        }
+
         return (
             <Parallax className="Scroll" y={[-50, 80]} tagOuter="figure">
-                <div className="ContentPane">
-                    <h1>{this.props.title}</h1>
-                    <p>{this.props.content}</p>
+                <div className="ContentPane" style={{flexDirection: imageSide, textAlign: textAnchor}}>
+                    <div className="ContentImage"></div>
+                    <div className="Text">
+                        <h1>{this.props.title}</h1>
+                        <p>{this.props.content}</p>
+                    </div>
                 </div>
             </Parallax> 
-        )
+        )   
     }
 }
 
